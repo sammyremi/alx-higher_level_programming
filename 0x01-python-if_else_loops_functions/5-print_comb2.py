62136dbfca5f6e1d70b2a8b4214b7917ff211ae4
Sammyremi <adebayorsamuelas@gmail.com>
2022-11-29 13:17:54 -0800

    5-print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@
 for x in range(10):
     for y in range(10):
         if y != 9 or x !=9 :
-            print (x, y, end=", ")
+            print("{}{}".format(x, y), end=", ")
         else:
-            print (x, y, end="\n")
+            print("{}{}".format(x, y), end="\n")
