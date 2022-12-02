027100ef15e6ec0695ce8e5a46d57879e58d9673
Sammyremi <adebayorsamuelas@gmail.com>
2022-12-01 17:20:16 -0800

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+    my_list = sys.argv
+    lent = len(my_list)
+
+    if lent == 1:
+        print("{} arguments.".format(lent - 1))
+
+    if lent == 2:
+        print("{} argument:".format(lent - 1))
+
+    if lent > 2:
+        print("{} arguments:".format(lent - 1))
+        for x in range(1, lent):
+            print("{}: {}".format(x, my_list[x]))
