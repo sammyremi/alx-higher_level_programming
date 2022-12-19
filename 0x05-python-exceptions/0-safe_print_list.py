f0aad669a3afcb9906f9c9d206c11136bde2ce4f
Sammyremi <adebayorsamuelas@gmail.com>
2022-12-19 10:37:48 -0800

    safe list print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,10 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[y], end="")
             count += 1
-        except:
+        except IndexError:
             break
     print()
     return count
-        
-        
-     
