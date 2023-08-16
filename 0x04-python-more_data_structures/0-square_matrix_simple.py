43f5dc9b8aa9f7c6cfe29152db07cd33d7f33e91
sammyremi <adebayorsamuelas@gmail.com>
2023-08-16 11:32:27 +0100

    search and replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
-def square_matrix_simple(matrix=[])
-    result = []
-    for items in matrix:
-        new_list = []
-        for x in items:
-            ans = x * x
-            new_list.append(ans)
-        result.append(new_list)
-    print(result)
-    print(matrix)
+def square_matrix_simple(matrix=[]):
+    return [list(map((lambda x: x * x), elm)) for elm in matrix]
